5b284f7c1d5272c84673eb86fa23e269963b3b59
Alvarezsergio <alvarez.94.sergio@gmail.com>
2024-07-11 20:58:14 -0300

    Format code and eliminate comments

--- index.js
+++ index.js
@@ -1,50 +1,48 @@
 // @ts-check 
 
-const url = 'https://jsonplaceholder.typicode.com/posts' //fake api
+const url = 'https://jsonplaceholder.typicode.com/posts' 
 const inputs = document.querySelectorAll("input");
 const buttons = document.querySelectorAll("button");
 const form = document?.querySelector("form")
 const select = document?.querySelector("select")
 
 
-
 async function handleSubmit(event) {
     event.preventDefault();
-    
+
     const data = {
-        name : event.target["name"].value,
-        lastname : event.target["lastname"].value,
-        email : event.target["email"].value,
-        date : event.target["dateOfBirth"].value,
-        country : event.target["country"].value,
-    
+        name: event.target["name"].value,
+        lastname: event.target["lastname"].value,
+        email: event.target["email"].value,
+        date: event.target["dateOfBirth"].value,
+        country: event.target["country"].value,
+
     };
     const errors = validate(data)
-    if(errors.length){
+    if (errors.length) {
         return alert("error en el formulario")
-        
     }
-   
-        
+
+
     const response = await fetch(url, {
-        method: "POST", // *GET, POST, PUT, DELETE, etc.    
-        body: JSON.stringify(data), // body data type must match "Content-Type" header
+        method: "POST", 
+        body: JSON.stringify(data), 
     })
-    
-    const {id} = await response.json();
+
+    const { id } = await response.json();
     if (id) alert("Formulario exitoso")
     else alert("error")
 }
 
-function validate(data){    
-    let errors = []  
-    if(!isNaN(data.name)){
-      errors.push("Error en campo nombre")
-    }  
-    if(!isNaN(data.lastname)){
-      errors.push("Error en campo apellid0")
-    }    
-    return errors;   
+function validate(data) {
+    let errors = []
+    if (!isNaN(data.name)) {
+        errors.push("Error en campo nombre")
+    }
+    if (!isNaN(data.lastname)) {
+        errors.push("Error en campo apellid0")
+    }
+    return errors;
 }
 
 function darkMode() {
